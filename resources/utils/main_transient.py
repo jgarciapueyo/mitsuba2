3632129f407a1e94079950081fe09db3308a0cfc
jgarciapueyo <jgarciapueyo@gmail.com>
2021-08-15 13:31:16 +0200

    add: python tool to process streak images

--- resources/utils/main_transient.py
+++ resources/utils/main_transient.py
@@ -1,11 +1,13 @@
+import argparse
+import glob
+from typing import Tuple
+
+import cv2 as cv
 import imageio
 import numpy as np
-import cv2 as cv
-import glob
-import mitsuba
 import matplotlib.pyplot as plt
-import OpenEXR
-import Imath
+
+import mitsuba
 
 mitsuba.set_variant("scalar_rgb")
 
@@ -14,7 +16,7 @@ def read_steadyimg_mitsuba(dir: str, extension: str = "exr") -> np.array:
     from mitsuba.core import Bitmap, Struct, float_dtype
     # other = Bitmap(f"{dir}.{extension}") \
     #     .convert(Bitmap.PixelFormat.XYZA, Struct.Type.Float32, srgb_gamma=False)
-    other = Bitmap(f"{dir}.{extension}")\
+    other = Bitmap(f"{dir}.{extension}") \
         .convert(Bitmap.PixelFormat.RGBA, Struct.Type.Float32, srgb_gamma=False)
     img = np.array(other, copy=False)
     return img
@@ -50,7 +52,7 @@ def read_streakimg_mitsuba(dir: str, extension: str = "exr") -> np.array:
     number_of_xtframes = len(glob.glob(f"{dir}/frame_*.{extension}"))
     fileList = []
     for i_xtframe in range(number_of_xtframes):
-        other = Bitmap(f"{dir}/frame_{i_xtframe}.{extension}")\
+        other = Bitmap(f"{dir}/frame_{i_xtframe}.{extension}") \
             .convert(Bitmap.PixelFormat.RGBA, Struct.Type.Float32, srgb_gamma=False)
         img = np.array(other, copy=False)
         fileList.append(np.expand_dims(img, axis=0))
@@ -68,82 +70,114 @@ def streakimg2steadyimg(streakimg: np.array) -> np.array:
     """
     return np.sum(streakimg, 2)
 
-if __name__ == "__main__":
-    # READ IMG MITSUBA
-    print("READ STREAK IMG MITSUBA")
-    steadyimg_mitsuba = read_steadyimg_mitsuba("../../resources/data/scenes/cbox/cbox", extension="exr")
-    steadyimg_mitsuba = steadyimg_mitsuba[:,:,:3]
-    print(steadyimg_mitsuba.shape)
-    print(steadyimg_mitsuba.dtype)
-    print(np.count_nonzero(np.isfinite(steadyimg_mitsuba[:, :, :3])))
 
-    # READ STREAK IMG AND ACCUMULATE
-    print("READ STREAK IMG")
-    streakimg_mitsuba_original = read_streakimg_mitsuba("../../resources/data/scenes/cbox/cbox_transient", extension="exr")
-    print("Original Streak: Min Value: " + str(np.amin(streakimg_mitsuba_original[:,:,:,:3], axis=(0,1,2))) + " - Max Value: " + str(np.amax(streakimg_mitsuba_original[:,:,:,:3], axis=(0,1,2))))
+def diff_images(img1: np.array, img2: np.array):
+    """
+    Shows the difference in values between both images.
+    :param img1:
+    :param img2:
+    :return:
+    """
+    diff = streakimg_mitsuba - steadyimg_mitsuba
+    number_of_channels = diff.shape[2]
+    fig, axs = plt.subplots(number_of_channels)
+    for channel in range(number_of_channels):
+        axs[channel].imshow(diff[:, :, channel])
+        axs[channel].colorbar()
+    plt.show()
+
+
+def tonemap_wrapper(tonemap: cv.Tonemap, img_hdr_RGB: np.array) -> Tuple[np.array, np.array]:
+    """
+    Tonemap wrapper from HDR image with RGB format to LDR images with format RGB and BGR format. The values of the LDR
+    images are in range [0, 1].
+
+    :param tonemap:
+    :param img:
+    :return:
+    """
+    img_ldr_RGB = tonemap.process(img_hdr_RGB)
+    img_ldr_BGR = cv.cvtColor(img_ldr_RGB, code=cv.COLOR_RGB2BGR)
+    return img_ldr_RGB, img_ldr_BGR
+
+
+def write_video(streakimg_hdr_RGB: np.array, out_file: str, tonemap: cv.Tonemap):
+    """
+    Creates a video from a HDR streak image (dimensions [height, width, time, channels]) in RGB format. The tonemap is
+    needed to transform the HDR streak image to a LDR streak image.
+
+    :param streakimg_hdr:
+    :param tonemap:
+    """
+    # 1. Get the streak image (already done) and define the output
+    writer = imageio.get_writer(out_file + ".mp4", fps=10)
+    # 2. Iterate over the streak img frames
+    for i in range(streakimg_hdr_RGB.shape[2]):
+        img_temp = streakimg_hdr_RGB[:, :, i, :3]
+        img_hdr_RGB = img_temp
+        img_ldr_RGB = tonemap.process(img_hdr_RGB)
+        writer.append_data((img_ldr_RGB * 255).astype(np.uint8))
+    # 3. Write the video
+    writer.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Tool for Streak Images")
+    parser.add_argument('-d', '--dir', type=str, help="Directory where the steady and streak images are stored",
+                        default="cbox")
+    parser.add_argument('-fs', '--file-steady-image', type=str, help="Name of the image file (for steady image)",
+                        default="scene")
+    parser.add_argument('-ft', '--file-streak-image', type=str, help="Name of the folder containing the single images "
+                                                                     "that compose the streak image",
+                        default="transient_scene")
+    parser.add_argument('-e', '--extension', type=str, help="Name of the extension of the images", default="exr")
+    args = parser.parse_args()
+
+    path_steady_img = args.dir + "/" + args.file_steady_image
+    path_streak_img = args.dir + "/" + args.file_streak_image
+
+    # Check if steady image exists for this scene
+    steady_img_exists = False
+    if glob.glob(path_steady_img + "." + args.extension):
+        steady_img_exists = True
+
+    # Read steady image
+    steadyimg_mitsuba = None
+    if steady_img_exists:
+        steadyimg_mitsuba = read_steadyimg_mitsuba(path_steady_img, extension=args.extension)
+        steadyimg_mitsuba = steadyimg_mitsuba[:, :, :3]  # drop alpha
+
+    # Read streak image
+    streakimg_mitsuba_original = read_streakimg_mitsuba(path_streak_img, extension=args.extension)
     streakimg_mitsuba = streakimg2steadyimg(streakimg_mitsuba_original)
     streakimg_mitsuba = streakimg_mitsuba[:, :, :3]  # drop alpha
-    print(streakimg_mitsuba_original.shape)
-    print(streakimg_mitsuba_original.dtype)
-    print(np.count_nonzero(np.isfinite(streakimg_mitsuba_original[:, :, :, :3])))
-    print(streakimg_mitsuba.shape)
-    print(streakimg_mitsuba.dtype)
-    print(np.count_nonzero(np.isfinite(streakimg_mitsuba[:, :, :3])))
 
-    # Comparison of streak (accumulated) and steady
-    if True:
-        diff = streakimg_mitsuba - steadyimg_mitsuba
-        plt.imshow(diff[:,:,0])
-        plt.colorbar()
-        plt.show()
-        plt.imshow(diff[:,:,1])
-        plt.colorbar()
-        plt.show()
-        plt.imshow(diff[:,:,2])
-        plt.colorbar()
-        # plt.show()
-    print("Steady: Min Value: " + str(np.amin(steadyimg_mitsuba[:,:,:3], axis=(0,1))) + " - Max Value: " + str(np.amax(steadyimg_mitsuba[:,:,:3], axis=(0,1))))
-    print("Streak: Min Value: " + str(np.amin(streakimg_mitsuba[:,:,:3], axis=(0,1))) + " - Max Value: " + str(np.amax(streakimg_mitsuba[:,:,:3], axis=(0,1))))
+    # Comparison of steady and streak (accumulated)
+    if steadyimg_mitsuba is not None:
+        diff_images(steadyimg_mitsuba, streakimg_mitsuba)
 
-    # TONEMAPPING AFTER SEEING THAT MITSUBA/OPENEXR/IMAGEIO are the same
-    print("TONEMAPPING")
+    # Tonemapping of HDR images
     tonemap = cv.createTonemapReinhard(gamma=2.2, light_adapt=0., color_adapt=0.)
-    # steadyimg_mitsuba_RGB = cv.cvtColor(steadyimg_mitsuba, code=cv.COLOR_XYZ2RGB)
-    steadyimg_mitsuba_RGB = steadyimg_mitsuba
-    steadyimg_mitsuba_ldr_RGB = tonemap.process(steadyimg_mitsuba_RGB)
-    steadyimg_mitsuba_ldr_BGR = cv.cvtColor(steadyimg_mitsuba_ldr_RGB, code=cv.COLOR_RGB2BGR)
+    steadyimg_mitsuba_ldr_RGB = None
+    steadyimg_mitsuba_ldr_BGR = None
+    if steadyimg_mitsuba:
+        [steadyimg_mitsuba_ldr_RGB, steadyimg_mitsuba_ldr_BGR] = tonemap_wrapper(tonemap, steadyimg_mitsuba)
+    [streakimg_mitsuba_ldr_RGB, streakimg_mitsuba_ldr_BGR] = tonemap_wrapper(tonemap, streakimg_mitsuba)
 
-    # TONEMAPPING STREAKIMG accumulated
-    # streakimg_mitsuba_RGB = cv.cvtColor(streakimg_mitsuba, code=cv.COLOR_XYZ2RGB)
-    streakimg_mitsuba_RGB = streakimg_mitsuba
-    streakimg_mitsuba_ldr_RGB = tonemap.process(streakimg_mitsuba_RGB)
-    streakimg_mitsuba_ldr_BGR = cv.cvtColor(streakimg_mitsuba_ldr_RGB, code=cv.COLOR_RGB2BGR)
+    # Show tonemapped images
+    if steadyimg_mitsuba:
+        cv.imshow("Steady Image", steadyimg_mitsuba_ldr_BGR)
+    cv.imshow("Streak Image (Accumulated)", streakimg_mitsuba_ldr_BGR)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-    # SHOW THE IMGS
-    if True:
-        cv.imshow("STEADY", steadyimg_mitsuba_ldr_BGR)
-        cv.imshow("ACCUMULATED", streakimg_mitsuba_ldr_BGR)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+    # Write LDR images
+    name_steadyimg_file = args.dir + "/steadyimg.png"
+    name_streakimg_file = args.dir + "/streakimg_accumulated.png"
+    if steadyimg_mitsuba:
+        imageio.imwrite(name_steadyimg_file, (steadyimg_mitsuba_ldr_RGB * 255).astype(np.uint8))
+    imageio.imwrite(name_streakimg_file, (streakimg_mitsuba_ldr_RGB * 255).astype(np.uint8))
 
-    # exit()
-    # WRITING THE IMGS
-    imageio.imwrite("../../resources/data/scenes/cbox/cbox_processed_RGB.png", (steadyimg_mitsuba_ldr_RGB * 255).astype(np.uint8))
-    imageio.imwrite("../../resources/data/scenes/cbox/cbox_accumulated_RGB.png", (streakimg_mitsuba_ldr_RGB * 255).astype(np.uint8))
-
-    # WRITING THE VIDEO
-    if False:
-        # 1. Get the streak image (already done) and define the output
-        writer = imageio.get_writer("../../resources/data/scenes/cbox/cbox_transient.mp4", fps=10)
-        # 2. Iterate over the streak img
-        for i in range(streakimg_mitsuba_original.shape[2]):
-            img = np.squeeze(streakimg_mitsuba_original[:, :, i, :3])
-            img_RGB = img
-            img_ldr_RGB = tonemap.process(img_RGB)
-            img_ldr_BGR = cv.cvtColor(img_ldr_RGB, cv.COLOR_RGB2BGR)
-            """
-            cv.imshow("ACCUMULATED", (img_ldr_BGR * 255).astype(np.uint8))
-            cv.waitKey(0)
-            """
-            writer.append_data((img_ldr_RGB * 255).astype(np.uint8))
-        writer.close()
+    # Write video of streak image
+    name_video_file = args.dir + "/streak_video"
+    write_video(streakimg_mitsuba_original, name_video_file, tonemap)
